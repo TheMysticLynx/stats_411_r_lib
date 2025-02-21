5557b03d19de8a24388831d63563d64dbf4f7f21
Kaiden Howard <kaidenghoward2004@gmail.com>
2025-02-20 20:25:02 -0800

    fix: add trivial case

--- R/combos.R
+++ R/combos.R
@@ -1,12 +1,47 @@
-fact <- function(n) {
-  if (n < 2) {
+#' Calculate permutations, N choose R / N p R
+#'
+#' @param n The total number of items
+#' @param r The number of items to choose
+#' @return The number of permutations
+#' @export
+perm <- function(n, r) {
+  if (n < r) {
+    return("r cannot be greater than n!")
+  }
+
+  if (n < 0 || r < 0) {
+    return("n and r must be positive integers!")
+  }
+
+  if (r == 0) {
     return(1)
   }
 
-  total <- 1
-  for (i in 1:n) {
-    total <- total * i
+  factorial(n) / factorial(n - r)
+}
+
+#' Calculate combinations, N choose R / N c R
+#'
+#' @param n The total number of items
+#' @param r The number of items to choose
+#' @return The number of combinations
+#' @export
+comb <- function(n, r) {
+  if (n < r) {
+    return("r cannot be greater than n!")
   }
 
-  total
+  if (n < 0 || r < 0) {
+    return("n and r must be positive integers!")
+  }
+
+  if (r == 0) {
+    return(1)
+  }
+
+  if (r == 1) {
+    return(n)
+  }
+
+  factorial(n) / (factorial(r) * factorial(n - r))
 }
