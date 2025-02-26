19a643a808a40945b2a97a89c056046c5b8a265a
Kaiden Howard <kaidenghoward2004@gmail.com>
2025-02-26 13:50:49 -0800

    feat: add uniforms

--- R/drv.R
+++ R/drv.R
@@ -1,3 +1,58 @@
+#' Find the mean of a Bernoulli distribution
+#'
+#' @param p Probability of success
+#' @return Mean of the Bernoulli distribution
+#' @export
+ber_mean <- function(p) {
+  p
+}
+
+#' Find the variance of a Bernoulli distribution
+#'
+#' @param p Probability of success
+#' @return Variance of the Bernoulli distribution
+#' @export
+ber_var <- function(p) {
+  p * (1 - p)
+}
+
+#' Find the standard deviation of a Bernoulli distribution
+#'
+#' @param p Probability of success
+#' @return Standard deviation of the Bernoulli distribution
+#' @export
+ber_sd <- function(p) {
+  sqrt(ber_var(p))
+}
+
+#' Find the mean of a uniform distribution
+#'
+#' @param x set of values
+#' @return Mean of the uniform distribution
+#' @export
+uniform_mean <- function(x) {
+  sum(x / length(x))
+}
+
+#' Find the variance of a uniform distribution
+#'
+#' @param x set of values
+#' @return Variance of the uniform distribution
+#' @export
+uniform_var <- function(x) {
+  mean <- uniform_mean(x)
+  sum(x^2) / length(x) - mean^2
+}
+
+#' Find the standard deviation of a uniform distribution
+#'
+#' @param x set of values
+#' @return Standard deviation of the uniform distribution
+#' @export
+uniform_sd <- function(x) {
+  sqrt(uniform_var(x))
+}
+
 #' Find the mean of a discrete distribution
 #'
 #' @param val Values of the discrete distribution
